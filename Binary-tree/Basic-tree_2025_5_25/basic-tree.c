d7bf51840cfa516444fe5970d5269ee6f1012ed1
nack <nack@192.168.1.34>
2025-05-25 22:44:59 +0700

    new_os

---
+++ Binary-tree/Basic-tree_2025_5_25/basic-tree.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+  int data;
+  struct node * left;
+  struct node * right;
+}node;
+
+node* createnode(int data){
+  node* temp = malloc(sizeof(node));
+  temp->data = data;
+  temp->left = NULL;
+  temp->right = NULL;
+  return temp;
+}
+
+int main(){
+  node * root = createnode(9);
+  return 0;
+}
