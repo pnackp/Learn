5515a86d445dc3a31cd985ead48f1ae019199681
unknown <pnackp@gmail.com>
2025-08-11 16:53:53 +0700

    graph_direct

---
+++ 7.graph/2.direct_graph/direct_graph.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define V 4
+
+void addEdge(uint8_t mat[V][V], uint8_t i, uint8_t j) {
+    mat[i][j] = 1;  
+}
+
+int main(){
+    uint8_t mat[V][V] = {0};
+    addEdge(mat,0,1);
+    addEdge(mat,1,3);
+    addEdge(mat,3,2);
+    for(uint8_t i = 0 ; i < V ; i++){
+        for(uint8_t j = 0 ; j < V ; j++){
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+    }
+}
